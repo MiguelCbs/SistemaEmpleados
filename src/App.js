cccde13bd21658c8ab0809e51b5d12317e76c8e5
Miguel Ceballos <38502446+MiguelCbs@users.noreply.github.com>
2024-05-10 17:05:50 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
 import { Route, Link, Routes, useNavigate, Navigate } from "react-router-dom";
-import Home from "./Components/Home";
-import Personal from "./Components/Personal";
-import Organigrama from "./Components/Organigrama";
-import Login from "./Components/Login";
-import Empleados from "./Components/Empleados";
+import Home from "./components/Home";
+import Personal from "./components/Personal";
+import Organigrama from "./components/Organigrama";
+import Login from "./components/Login";
+import Empleados from "./components/Empleados";
 import "react-datepicker/dist/react-datepicker.css";
 import { Switch } from "@mui/material";
-
-
 function App() {
   const [currentRotation, setCurrentRotation] = useState(0);
   const [isAuthenticated, setIsAuthenticated] = useState(false); // Asumiendo que esto se maneja correctamente
   const navigate = useNavigate();
   const [message, setMessage] = useState('');
+  
+//Se introduce un estado para rastrear si la verificación de auntenticación aún esta en progreso...
+//...cuando se comprueba que el usuario esta auntenticado se renderiza o redirige el componente de la ruta
+  const ProtectedRoute = ({ component: Component, ...rest }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAuthChecking, setIsAuthChecking] = useState(true);
+  
+    useEffect(() => {
+      const verifyAuth = async () => {
+        const token = localStorage.getItem('access_token');
+        setIsAuthenticated(!!token);
+        setIsAuthChecking(false);
+      };
+      verifyAuth();
+    }, []);
+  
+    if (isAuthChecking) {
+      return <div>Loading...</div>;
+    }
+    console.log("Autenticado:", isAuthenticated);
+    return isAuthenticated ? <Component {...rest} /> : <Navigate to="/" />;
+  };
 
-
-const ProtectedRoute = ({ isAuthenticated, component: Component, ...rest }) => {
-  console.log("Autenticado:", isAuthenticated);
-  return isAuthenticated ? <Component {...rest} /> : <Navigate to="/" />;
-};
-
-
+useEffect(() => {
+  const token = localStorage.getItem('access_token');
+  setIsAuthenticated(!!token);
+}, []);
   const images = [
     "/static/img/img1.jpg",
     "/static/img/img2.jpg",
@@ -32,9 +49,42 @@ const ProtectedRoute = ({ isAuthenticated, component: Component, ...rest }) => {
     "/static/img/img6.jpg",
   ];
   //Display control 2
- 
+  useEffect(() => {
+    const menuIcon = document.querySelector("#menu-icon");
+    const navbar = document.querySelector(".navbar");
+    if (menuIcon) {
+      menuIcon.onclick = () => {
+        menuIcon.classList.toggle("bx-x");
+        navbar.classList.toggle("active");
+      };
+    }
+  }, []);
 
+  //Display control
+  let sections = document.querySelectorAll("section");
+  let navLinks = document.querySelectorAll("header nav a");
 
+  window.onscroll = () => {
+    sections.forEach((sec) => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 100;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute("id");
+
+      if (top >= offset && top < offset) {
+        navLinks.forEach((links) => {
+          links.classList.remove("active");
+          document
+            .querySelector("header nav a[href*=" + id + "]")
+            .classList.add("active");
+        });
+      }
+    });
+
+    let header = document.querySelector("header");
+
+    header.classList.toggle("sticky", window.scrollY > 100);
+  };
 
   function handlePrevClick() {
     setCurrentRotation(currentRotation + 360 / images.length);
@@ -45,7 +95,13 @@ const ProtectedRoute = ({ isAuthenticated, component: Component, ...rest }) => {
   }
 
   const handleLogout = () => {
-    setIsAuthenticated(false);
+    localStorage.removeItem('access_token'); // Eliminar el token del almacenamiento local
+    setIsAuthenticated(false); // Actualizar el estado de autenticación a falso
+    navigate('/Login'); // Redirigir al usuario a la página de login
+};
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
 return (
@@ -55,7 +111,7 @@ return (
         Cibercom
       </Link>
       <div>
-        <div  id="menu-icon"color="var(--text-color)" className="bx bx-menu"></div>
+      <div  id="menu-icon"color="var(--text-color)" className="bx bx-menu"></div>
       </div>
       {isAuthenticated && (
       <nav className="navbar">
@@ -65,6 +121,7 @@ return (
         <Link to="/Organigrama">Organigrama</Link>
         <Link to="/Empleados">Empleados</Link>
         
+         
         {isAuthenticated ? (
         <button onClick={handleLogout}>Logout</button>
       ) : (
@@ -77,10 +134,8 @@ return (
     </header>
 
     <Routes>
-    <Route path="/" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-    {isAuthenticated && (
-    <>
-    <Route path="/Home" element={
+      <Route path="/Login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+      <Route path="/Home" element={
           <Home
             currentRotation={currentRotation}
             handlePrevClick={handlePrevClick}
@@ -88,25 +143,24 @@ return (
             images={images}
           />
         }/>
-      <Route path="/Organigrama" element={<Organigrama />}/>
-      <Route path="/Empleados" element={<Empleados />} />
-      <Route path="/Personal/:id" element={<Personal />} /> {}
-      </>
-      )}
-      <Route path="*" element={<Navigate replace to="/" />} />
+      <Route path="/Organigrama" element={<ProtectedRoute isAuthenticated={isAuthenticated} component={Organigrama} />} />
+      <Route path="/Empleados" element={<ProtectedRoute isAuthenticated={isAuthenticated} component={Empleados} />} />
+      <Route path="/Personal/:id" element={<ProtectedRoute isAuthenticated={isAuthenticated} component={Personal} />} />
+      <Route path="*" element={<Navigate to={isAuthenticated ? "/Home" : "/Login"} replace />} />
     </Routes>
 
     <footer className="footer">
-        <div className="footer-text">
-          <p>Copyright &copy; 2023 by Cibercom | All Rights Reserved.</p>
-        </div>
-        <div className="footer-iconTop">
-          <a href="/">
-            <box-icon name="up-arrow-alt"></box-icon>
-          </a>
-        </div>
-      </footer>
-    </div>
-  );
+      <div className="footer-text">
+        <p>Copyright &copy; 2023 by Cibercom | All Rights Reserved.</p>
+      </div>
+      <div className="footer-iconTop">
+        <a href="#top" onClick={scrollToTop}>
+          <box-icon name="up-arrow-alt"></box-icon>
+        </a>
+      </div>
+    </footer>
+    <script src="./js/script.js"></script>
+  </div>
+);
 }
 export default App;
